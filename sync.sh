750419751211780a8f006e86b7a305324f065cf5
MuYu <mr.muzea@gmail.com>
2019-10-20 17:59:15 +0800

    fix: sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 br=`git symbolic-ref --short -q HEAD | awk -F 'go' '{print $2}'`
-sed -i s/0.0.0/$br/ package.json
+case "$OSTYPE" in
+  darwin*)  sed -i '' "s/0.0.0/$br.0/" "package.json" ;;
+  linux*)   sed -i    "s/0.0.0/$br.0/" "package.json" ;;
+esac
+mkdir dist
 curl https://raw.githubusercontent.com/golang/go/release-branch.go$br/misc/wasm/wasm_exec.js > ./dist/wasm_exec.js
-
